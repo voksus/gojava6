cdcfae6fbd5921e0e8f932687a6f1d666784f5aa
voksus <voksus@gmail.com>
2017-02-05 00:36:51 +0200

    New: task 2.2

---
+++ src/module_02/homeWork/task22.java
@@ -0,0 +1,23 @@
+package module_02.homeWork;
+
+/**
+ * Created by voksus on 30.01.2017.
+ */
+public class task22 {
+
+    public static void main(String[] args) {
+
+        System.out.println(withdraw(100, 10));
+    }
+
+    private static String withdraw(double balance, double withdrawal) {
+        StringBuilder result = new StringBuilder();
+        if(balance > 0.0 && withdrawal > 0.0 && withdrawal * 1.05 > balance) {
+            result.append("NO");
+        } else {
+            result.append("OK ").append(Double.toString(withdrawal * 0.05)).append(" ");
+            result.append(Double.toString(balance - withdrawal * 1.05));
+        }
+        return result.toString();
+    }
+}
