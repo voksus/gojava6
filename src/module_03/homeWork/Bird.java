9665c0b749b8a6d0a7df104e30f5f52d148281f7
voksus <voksus@gmail.com>
2017-02-07 22:49:21 +0200

    NEW: task 3.1

---
+++ src/module_03/homeWork/Bird.java
@@ -0,0 +1,22 @@
+package module_03.homeWork;
+
+/**
+ * Created by voksus on 07.02.2017.
+ */
+public class Bird {
+
+    public static void main(String[] args) {
+
+        Bird bird = new Bird();
+
+        bird.sing();
+
+    }
+
+    private static void sing() {
+        System.out.println("I am walking");
+        System.out.println("I am flying");
+        System.out.println("I am singing");
+        System.out.println("I am Bird");
+    }
+}
