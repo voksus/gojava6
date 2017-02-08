116c7cc64ce82855a5a6b994c00c4067f132d287
voksus <voksus@gmail.com>
2017-02-08 22:40:53 +0200

    Update3: task 3.3

--- src/module_03/homeWork/task33/Solution.java
+++ src/module_03/homeWork/task33/Solution.java
@@ -9,24 +9,27 @@ public class Solution {
 
     public static void main(String[] args) {
 
-        Course course1 = new Course(new Date(453453212003L), "Vasya");
-        Course course2 = new Course(100, "Petya", "Marivanna");
+        Course course1 = new Course(new Date(117, 0, 20), "Vasya");
+        Course course2 = new Course(new Date(117, 0, 20), "Petya");
+        Course course3 = new Course(100, "Igor", "Marivanna");
+        Course course4 = new Course(100, "Sonya", "Marivanna");
+        Course course5 = new Course(100, "Natali", "Marivanna");
 
         Student student1 = new Student("Fedya", "Pupkin", 103);
-        Student student2 = new Student("Kozlov", new Course[]{course1, course2});
+        Student student2 = new Student("Kozlov", new Course[]{course3, course4});
 
         CollegeStudent collegeStudent1 = new CollegeStudent("Ivanidze",
                 "Balishvili", 103);
-        CollegeStudent collegeStudent2 = new CollegeStudent("Pepa", new Course[]{course1, course2});
+        CollegeStudent collegeStudent2 = new CollegeStudent("Pepa", new Course[]{course3, course4, course5});
         CollegeStudent collegeStudent3 = new CollegeStudent("Yom", "Li",
-                103, new Course[]{course1, course2}, 25,
+                103, new Course[]{course3}, 25,
                 "Cambridge", 5, 24872345762764L);
 
         SpecialStudent specialStudent1 = new SpecialStudent("Ali", "Abu", 103);
-        SpecialStudent specialStudent2 = new SpecialStudent("Kolindin", new Course[]{course1, course2});
+        SpecialStudent specialStudent2 = new SpecialStudent("Kolindin", new Course[]{course3});
         SpecialStudent specialStudent3 = new SpecialStudent("Zhmodya", "Pukin",
-                103, new Course[]{course1, course2}, 37,
-                "KPI", 3, 76542098576L, 12345678900L)
+                103, new Course[]{course3, course4, course5}, 37,
+                "KPI", 3, 76542098576L, 12345678900L);
     }
 
 }
