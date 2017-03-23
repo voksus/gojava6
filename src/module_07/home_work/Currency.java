43e08020f4cf0d6d42a913af4b55d54fa37daf37
voksus <voksus@gmail.com>
2017-03-23 18:06:51 +0200

    NEW module 7

---
+++ src/module_07/home_work/Currency.java
@@ -0,0 +1,8 @@
+package module_07.home_work;
+
+/**
+ * Created by voksus on 20.03.2017.
+ */
+public enum Currency {
+    USD, EUR;
+}
---
+++ src/module_07/home_work/Order.java
@@ -0,0 +1,13 @@
+package module_07.home_work;
+
+/**
+ * Created by voksus on 20.03.2017.
+ */
+public class Order {
+    long id;
+    int price;
+    Currency currency;
+    String itemName;
+    String shopIdentificator;
+    User user;
+}
