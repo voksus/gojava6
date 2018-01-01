8e8eb25e69661579c133a823804db65754422485
voksus <voksus@gmail.com>
2018-01-01 18:24:23 +0200

    palindrome : 2nd cleanup code

--- src/my_tasks/palindrome/MaxPalindromeSearch.java
+++ src/my_tasks/palindrome/MaxPalindromeSearch.java
@@ -1,55 +1,34 @@
 package my_tasks.palindrome;
 
 /**
- * @author Volodymyr Burtsev at 29.12.2017 13:59
+ * @author Volodymyr Burtsev at 29.12.2017 16:59
  */
 public class MaxPalindromeSearch {
 
-    private static final int MIN_NUMBER = 10_000;
-    private static final int MAX_NUMBER = 99_999;
-    private static int[] primeNumbers;
-    private static int countPrimeNumbers = 0;
-
-    private static int multiplier1;
-    private static int multiplier2;
-    private static long palindrome;
-    private static int palindromesCount;
-
-    private static final boolean showDetails = false;
-    private static final boolean useBenchmark = false;
-    private static long timer;
+    private static long timer;                                  // to check used time
+    private static boolean showMoreInformation = false;         // to show additional info
+    private static boolean showTotalPalindromesCount = false;    // to count all possible palindromes
 
     public static void main(String[] args) {
 
-        getPrimeNumbers();
+        int[] primeNumbers = getPrimeNumbers(10_000, 99_999);
 
-        benchmark();
-
-        findBiggestPalindromeByMultiplying(primeNumbers);
+        findBiggestPalindrome(primeNumbers);
 
     }
 
-    private static void timerStart() {
-        timer = System.currentTimeMillis();
-    }
-
-    private static void timerStop() {
-        System.out.println((System.currentTimeMillis() - timer) + "ms");
-    }
-
-    private static void getPrimeNumbers() {
-        System.out.println("\nSearching all prime numbers in range " + MIN_NUMBER + ".." + MAX_NUMBER);
+    private static int[] getPrimeNumbers(int minNumber, int maxNumber) {
+        if (minNumber < 2 || minNumber > maxNumber) throw new RuntimeException("Wrong range numbers. You can use range from 2 to Integer.MAX_VALUE");
+        System.out.println("\nSearching all prime numbers in range " + minNumber + ".." + maxNumber);
         System.out.print("Time used to search all prime numbers: ");
-        if (primeNumbers != null) {
-            countPrimeNumbers = 0;
-        }
-        primeNumbers = new int[MAX_NUMBER];
+
         timerStart();
-        if (countPrimeNumbers == 0) {
-            primeNumbers[0] = 2;
-            countPrimeNumbers++;
-        }
-        for (int i = 3; i <= MAX_NUMBER; i = i + 2) {
+        int[] primeNumbers = new int[maxNumber - minNumber + 1];
+        primeNumbers[0] = 2;    // 1st even and single prime number
+        int countPrimeNumbers = 1;
+
+        // search all prime numbers in range 3..maxNumber
+        for (int i = 3; i <= maxNumber; i = i + 2) {
             boolean isPrime = true;
             int limit = (int) Math.sqrt(i);
             for (int j = 0; j < limit; j++) {
@@ -66,60 +45,62 @@ public class MaxPalindromeSearch {
         System.arraycopy(primeNumbers, 0, result, 0, countPrimeNumbers);
         primeNumbers = result;
 
-        // select range using lower number
+        // trim selected range by lower value
         result = null;
         for (int i = 0; i < primeNumbers.length; i++) {
-            if (primeNumbers[i] >= MIN_NUMBER) {
+            if (primeNumbers[i] >= minNumber) {
                 result = new int[primeNumbers.length - i];
                 System.arraycopy(primeNumbers, i, result, 0, primeNumbers.length - i);
                 break;
             }
         }
-        primeNumbers = result;
         timerStop();
         System.out.println("\nTotal " + primeNumbers.length + " prime numbers found.\n--------");
-        showSelected(result);
+        return result;
     }
 
-    private static void findBiggestPalindromeByMultiplying(int[] primeNumbersInRange) {
+    private static void findBiggestPalindrome(int[] primeNumbersInRange) {
+
         timerStart();
-        palindrome = 0;
-        palindromesCount = 0;
-        multiplier1 = 0;
-        multiplier2 = 0;
-        System.out.println("\nSearching palindromes in range: " + MIN_NUMBER + ".." + MAX_NUMBER);
-        if (showDetails) System.out.println("Palindromes found:");
+
+        long palindrome = 0;
+        int palindromesCount = 0;
+        int multiplier1 = 0;
+        int multiplier2 = 0;
+
+        System.out.println("\nSearching palindromes in selected range...");
+        if (showMoreInformation) System.out.println("Palindromes found:");
         boolean palindromeFound;
         for (int i = primeNumbersInRange.length - 1; i >= 0; i--) {
             for (int j = i; j >= 0; j--) {
                 long n1 = primeNumbersInRange[i];
                 long n2 = primeNumbersInRange[j];
                 long numberToCheck = n1 * n2;
-                palindromeFound = isPalindrome_UsingString(numberToCheck);
-//                palindromeFound = isPalindrome_UsingChar(numberToCheck);
-//                palindromeFound = isPalindrome_UsingNumber(numberToCheck);
+                palindromeFound = isPalindrome(numberToCheck);
                 if (palindromeFound) {
                     palindromesCount++;
-                    if (showDetails) System.out.println("  " + palindromesCount + ") \t" + n1 + " x " + n2
+                    if (showMoreInformation) System.out.println("  " + palindromesCount + ") \t" + n1 + " x " + n2
                             + " = " + numberToCheck + "\t\t");
                     if (numberToCheck > palindrome) {
                         palindrome = numberToCheck;
                         multiplier1 = primeNumbersInRange[i];
                         multiplier2 = primeNumbersInRange[j];
                     }
-//                    break;  // optimize to ignore other guaranteed smaller possible palindromes when array is sorted
+                    // the line below will optimize calculations by ignoring other guaranteed smaller possible palindromes
+                    // but useless when total palindromes count needed
+                    if (!showTotalPalindromesCount) break;
                 }
             }
         }
         System.out.print("Time used for searching all palindromes: ");
         timerStop();
-        if (showDetails) System.out.println("Total palindromes found: " + palindromesCount);
+        if (showTotalPalindromesCount) System.out.println("Total palindromes found: " + palindromesCount);
         System.out.println("--------\n\n\nFinally biggest palindrom is:\n\t" + palindrome +
-                "\nwhich taken by multiplying the prime numbers from range " + MIN_NUMBER + ".." + MAX_NUMBER + " :\n\t" +
+                "\nwhich taken by multiplying the prime numbers from range:\n\t" +
                 multiplier1 + " and " + multiplier2 + "\n");
     }
 
-    private static boolean isPalindrome_UsingString(long numberToCheck) {
+    private static boolean isPalindrome(long numberToCheck) {
         String number = String.valueOf(numberToCheck);
         boolean isPalindrome = true;
         for (int i = 0; i < number.length() / 2; i++) {
@@ -129,123 +110,12 @@ public class MaxPalindromeSearch {
         return isPalindrome;
     }
 
-    private static boolean isPalindrome_UsingChar(long numberToCheck) {
-        boolean isPalindrome = true;
-        char[] chars = String.valueOf(numberToCheck).toCharArray();
-
-        for (int i = 0; i < chars.length / 2; i++) {
-            if (chars[i] != chars[chars.length - 1 - i]) {
-                isPalindrome = false;
-                break;
-            }
-        }
-        return isPalindrome;
+    private static void timerStart() {
+        timer = System.currentTimeMillis();
     }
 
-    private static boolean isPalindrome_UsingNumber(long numberToCheck) {
-        boolean isPalindrome = true;
-        int numbersCount = 0;
-        for (long i = numberToCheck; i > 0; i /= 10) {
-            numbersCount++;
-        }
-        long n1 = 0, n2 = 0;
-        for (int i = 0; i < numbersCount / 2; i++) {
-            n1 = (numberToCheck / pow10(numbersCount - i - 1) % 10);
-            n2 = (numberToCheck / pow10(i) % 10);
-            if (n1 != n2) {
-                isPalindrome = false;
-                break;
-            }
-        }
-        return isPalindrome;
+    private static void timerStop() {
+        System.out.println((System.currentTimeMillis() - timer) + "ms");
     }
 
-    private static void showSelected(int[] primeNumbersInRange) {
-        if (!showDetails) return;
-        System.out.println("\n\nSelected prime numbers in range " + MIN_NUMBER + ".." + MAX_NUMBER + " :");
-        int q = 0;
-        for (int i : primeNumbersInRange) {
-            if ((q %= 20) == 0) {
-                System.out.println();
-            }
-            System.out.print(i + "\t");
-            q++;
-        }
-        System.out.println("\n\nTotal " + primeNumbersInRange.length + " prime numbers in selected range\n--------\n");
-    }
-
-    private static void benchmark() {
-        if (!useBenchmark) return;
-        long minTime = 0;
-        long maxTime = 0;
-        long avgTime = 0;
-        int iterationsCount = 100;
-
-        System.out.println("\n--> Benchmark started");
-        if (showDetails) System.out.println("Iterations duration for " + iterationsCount + " times:");
-
-        int q = 0;
-        for (int i = 1; ; i++) {
-            if ((q %= 20) == 0) {
-                if (showDetails) System.out.println();
-            }
-            long timerStart = System.currentTimeMillis();
-            getPrimeNumbers();
-            long timeUsed = System.currentTimeMillis() - timerStart;
-            if (minTime > timeUsed || minTime == 0) minTime = timeUsed;
-            if (maxTime == 0 || maxTime < timeUsed) maxTime = timeUsed;
-            avgTime += timeUsed;
-            if (i < iterationsCount) {
-                primeNumbers = new int[MAX_NUMBER];
-                countPrimeNumbers = 0;
-            } else {
-                break;
-            }
-            q++;
-        }
-
-        if (showDetails) System.out.println("\n\nMinimum: " + minTime + "ms\nMaximum: " + maxTime
-                + "ms\nAverage: " + avgTime / iterationsCount + "ms");
-        System.out.println("\n--> Benchmark finished");
-    }
-
-    private static long pow10(int exponent) {
-        long result = 0;
-        switch (exponent) {
-            case 0:
-                result = 1L;
-                break;
-            case 1:
-                result = 10L;
-                break;
-            case 2:
-                result = 100L;
-                break;
-            case 3:
-                result = 1000L;
-                break;
-            case 4:
-                result = 10000L;
-                break;
-            case 5:
-                result = 100000L;
-                break;
-            case 6:
-                result = 1000000L;
-                break;
-            case 7:
-                result = 10000000L;
-                break;
-            case 8:
-                result = 100000000L;
-                break;
-            case 9:
-                result = 1000000000L;
-                break;
-            case 10:
-                result = 10000000000L;
-                break;
-        }
-        return result;
-    }
 }
